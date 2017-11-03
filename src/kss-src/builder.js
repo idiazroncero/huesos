954cd58615019a5888cbbe95e65e024f02df8d32
Nacho Diaz <idiaz.roncero@gmail.com>
2017-11-03 14:52:42 +0100

    Revamps docs

--- src/kss-src/builder.js
+++ src/kss-src/builder.js
@@ -129,9 +129,20 @@ class KssBuilderHandlebars extends KssBuilderBaseHandlebars {
         });
       }
 
+      this.Handlebars.registerHelper("debug", function(optionalValue) {
+        console.log("Current Context");
+        console.log("====================");
+        console.log(this);
+       
+        if (optionalValue) {
+          console.log("Value");
+          console.log("====================");
+          console.log(optionalValue);
+        }
+      });
+
       return Promise.resolve(styleGuide);
     });
   }
 }
-
 module.exports = KssBuilderHandlebars;
