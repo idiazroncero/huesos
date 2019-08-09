0856f0743906fd6980eff86bb3bd483676516378
Nacho Diaz <idiaz.roncero@gmail.com>
2019-08-09 14:14:57 +0200

    updates docs

--- docs/styleguide/huesos-mandlebrot.js
+++ docs/styleguide/huesos-mandlebrot.js
@@ -1,3 +1,25 @@
 $(function(){
-    alert('holi');
+    
+    $('.item--has-children').each(function(item, index){
+        var menu = $(this).children('.item__children');
+        menu.hide();
+
+        $(this).on('click', function(e){
+            e.stopPropagation();
+            menu.toggle();
+        })
+    })
+
+    $('.preview-tag').on('click', function(){
+        $(this).parent('.pen-preview').toggleClass('pen-preview--medium');
+    })
+
+    $('.is-current').each(function(){
+        $(this).parents('ul, li').show();
+    });
+
+
+    $('#toggle-grid').on('click', function(){
+        $('body').toggleClass('show-grid');
+    })
 })
